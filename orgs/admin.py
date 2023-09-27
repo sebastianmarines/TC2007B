78dac26b4f62203e5387270db09f74cf2be8f7fc
Sebastian Marines <a01383056@tec.mx>
2023-09-26 22:27:21 -0600

    Create OSC models

--- orgs/admin.py
+++ orgs/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Tag, OSC, Multimedia
 
-# Register your models here.
+admin.site.register(Tag)
+admin.site.register(OSC)
+admin.site.register(Multimedia)
--- orgs/models.py
+++ orgs/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    tagId = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField()
+
+
+class OSC(models.Model):
+    oscId = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    alias = models.CharField(max_length=100)
+    tags = models.ManyToManyField(Tag)
+    direccion = models.TextField()
+    coordenadas_latitud = models.FloatField()
+    coordenadas_longitud = models.FloatField()
+    email = models.EmailField()
+    telefono = models.CharField(max_length=20)
+    areaContacto = models.CharField(max_length=255)
+    horaAtencion = models.CharField(max_length=255)
+    paginaWeb = models.URLField()
+    redesSociales = models.JSONField()
+    videos = models.ManyToManyField('Multimedia', related_name='osc_videos', blank=True)
+    imagenes = models.ManyToManyField('Multimedia', related_name='osc_imagenes', blank=True)
+    articulosInteres = models.ManyToManyField('Multimedia', related_name='osc_articulos', blank=True)
+
+    class Meta:
+        verbose_name = 'OSC'
+        verbose_name_plural = 'OSCs'
+
+
+class Multimedia(models.Model):
+    url = models.URLField()
